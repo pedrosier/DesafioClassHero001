7ce679c9045b8a631e0771826cbc1f85c2bbb4c5
pedrosier <pedrosier001@gmail.com>
2025-08-24 18:04:52 -0300

    Arquivos desafio classhero
    arquivos de resolução

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crie um programa que declare quatro variáveis para armazenar as seguintes informações de uma pessoa:
+// o nome completo, a idade, a letra inicial do nome e o seu salário. 
+//Inicialize essas variáveis com dados de exemplo e exiba cada informação no console.
+
+
+string nomeCompleto = "Pedro Kings";
+int idade = 30;
+char letraInicialNome = 'P';
+double salario = 15000.50;
+
+Console.WriteLine("Nome Completo: " + nomeCompleto);
+Console.WriteLine("Idade: " + idade);
+Console.WriteLine("Letra Inicial: " + letraInicialNome);
+Console.WriteLine("Salário: " + salario);
